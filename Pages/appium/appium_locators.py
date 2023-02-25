109079f99991813a1f9045dffb4df4882e5958e3
BeliDzoni <belidzoni@gmail.com>
2023-02-25 12:49:06 +0100

    Android 11 locators update

--- Pages/appium/appium_locators.py
+++ Pages/appium/appium_locators.py
@@ -5,7 +5,7 @@ class GoogleMapsLocators:
     SEARCH_BOX = (AppiumBy.ID, "com.google.android.apps.maps:id/search_omnibox_text_box")
     EDIT_SEARCH_BOX = (AppiumBy.ID, "com.google.android.apps.maps:id/search_omnibox_edit_text")
     SELECT_LOCATION = (AppiumBy.XPATH, "//android.widget.TextView[contains()]")
-    DIRECTION_BTN = (AppiumBy.XPATH, '//android.widget.Button[contains(@content-desc,"Directions to")]')
+    DIRECTION_BTN = (AppiumBy.XPATH, '//*[contains(@content-desc,"Directions")]')
     FROM_DEST = (AppiumBy.ID, "com.google.android.apps.maps:id/directions_startpoint_textbox")
     TO_DEST = (AppiumBy.ID, "com.google.android.apps.maps:id/directions_endpoint_textbox")
     DISTANCE_VALUE = (AppiumBy.XPATH, '//*[@resource-id="com.google.android.apps.maps:id/sheet_header"]'
